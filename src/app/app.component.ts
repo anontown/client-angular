2b0b43eb0275c3bf2eea1f3615d813f27c0fd1fb
kgtkr <kgtkr.jp+program@gmail.com>
2016-12-26 16:28:24 +0900

    ナビゲーションクリックで閉じるようにした

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component, HostListener, ViewChild, ElementRef, OnInit } from '@angular/core';
+import {
+    Component,
+    HostListener,
+    ViewChild,
+    ElementRef,
+    OnInit
+} from '@angular/core';
+import { Router } from '@angular/router';
 import { AtApiService, AtConfig } from 'anontown';
 import { UserDataService } from './services';
-
+var $ = require('jquery');
 
 @Component({
     selector: 'app-root',
@@ -9,34 +16,32 @@ import { UserDataService } from './services';
         <nav class="navbar navbar-default navbar-fixed-top" (window:resize)="resize()" #nav>
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
+                    <a [routerLink]="['/']" class="navbar-brand">Anontown</a>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" id="navbar-toggle">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
-                    <a [routerLink]="['/']" class="navbar-brand">Anontown</a>
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
                         <li *ngIf="ud.isToken|async">
-                            <a [routerLink]="['/favo']">お気に入り</a>
+                            <a (click)="ok(['/favo'])">お気に入り</a>
                         </li>
                         <li>
-                            <a [routerLink]="['/topic/search']" [queryParams]="{title:'',category:''}">検索</a>
+                            <a (click)="ok(['/topic/search'],{title:'',category:''})">検索</a>
                         </li>
                         <li *ngIf="ud.isToken|async">
-                            <a [routerLink]="['/topic/write']">新規トピック</a>
+                            <a (click)="ok(['/topic/write'])">新規トピック</a>
                         </li>
                         <li *ngIf="ud.isToken|async">
-                            <a [routerLink]="['/user/notice']">通知</a>
+                            <a (click)="ok(['/user/notice'])">通知</a>
                         </li>
                         <li *ngIf="ud.isToken|async">
-                            <a [routerLink]="['/user/profile']">プロフ管理</a>
+                            <a (click)="ok(['/user/profile'])">プロフ管理</a>
                         </li>
                         <li *ngIf="ud.isToken|async">
-                            <a [routerLink]="['/user/msg']">お知らせ</a>
+                            <a (click)="ok(['/user/msg'])">お知らせ</a>
                         </li>
                     </ul>
                     <button *ngIf="ud.notToken|async" type="button" (click)="login()" class="btn btn-default navbar-btn navbar-right">ログイン</button>
@@ -53,8 +58,15 @@ export class AppComponent implements OnInit {
     resize() {
         document.body.style.paddingTop = this.nav.nativeElement.clientHeight + 20 + "px";
     }
-    constructor(private ud: UserDataService, private api: AtApiService) {
+    constructor(private ud: UserDataService,
+        private api: AtApiService,
+        private router: Router) {
         setInterval(() => this.save(), 30 * 1000);
+    }
+
+    ok(url: any[], params: any) {
+        this.router.navigate(url, { queryParams: params });
+        $("#navbar-toggle").click();
     }
 
     ngOnInit() {
