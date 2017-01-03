9af0365af8b5de57a958d26efaa6e62ad7cbae79
kgtkr <kgtkr.jp+program@gmail.com>
2017-01-03 19:02:02 +0900

    tslintに少し従った

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,40 +15,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
+  ud: IUserData;
+  private udListener: IUserDataListener;
+
   constructor(private user: UserService,
     private api: AtApiService,
     public router: Router) {
     setInterval(() => this.save(), 30 * 1000);
   }
 
-  ud: IUserData;
-  private udListener: IUserDataListener;
-
   async ngOnInit() {
-    //トークンリクエスト
+    // トークンリクエスト
     this.router.events.take(1).subscribe(async e => {
       await (async () => {
         let params = this.router.parseUrl(e.url).queryParams;
-        let id = params["id"];
-        let key = params["key"];
+        let id = params['id'];
+        let key = params['key'];
         if (id && key) {
           let token = await this.api.findTokenReq({
             id,
             key
           });
 
-          localStorage.setItem("token", JSON.stringify({
+          localStorage.setItem('token', JSON.stringify({
             id: token.id,
             key: token.key
           }));
         }
       })().catch(_e => {
-        //トークンリクエスト→トークンの変換失敗
+        // トークンリクエスト→トークンの変換失敗
       });
 
-      //認証
+      // 認証
       await (async () => {
-        let json = localStorage.getItem("token");
+        let json = localStorage.getItem('token');
         if (json) {
           let auth: IAuthToken = JSON.parse(json);
           let token = await this.api.findTokenOne(auth);
@@ -71,18 +71,18 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   login() {
-    location.href = Config.userURL + "/auth?client=" + Config.clientID;
+    location.href = Config.userURL + '/auth?client=' + Config.clientID;
   }
 
   logout() {
     this.user.setUserData(null);
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
   }
 
   @HostListener('window:beforeunload')
   beforeUnloadHander() {
     this.save();
-    return "ページを移動しますか？";
+    return 'ページを移動しますか？';
   }
 
   async save() {
--- src/app/storage.ts
+++ src/app/storage.ts
@@ -1,53 +1,61 @@
 import * as Immutable from 'immutable';
 
 interface StorageJSON {
-    ver: string
+    ver: string;
 }
 
-interface StorageJSON1_0_0 {
-    ver: "1.0.0",
-    topicFav: string[],
-    topicRead: { topic: string, res: string }[],
+interface StorageJSON1 {
+    ver: '1.0.0';
+    topicFav: string[];
+    topicRead: { topic: string, res: string }[];
 }
 
 interface StorageJSON2 {
-    ver: "2",
+    ver: '2',
     topicFav: string[],
     topicRead: { topic: string, res: string, count: number }[]
 }
 
 interface StorageJSON3 {
-    ver: "3",
+    ver: '3',
     topicFavo: string[],
     topicRead: { [key: string]: { res: string, count: number } };
 }
 
 export class Storage {
-    static VER: "3" = "3";
-    readonly topicFavo: Immutable.Set<string>;
-    readonly topicRead: Immutable.Map<string, { res: string, count: number }>;
+    static VER: '3' = '3';
 
-    private constructor(topicFavo: Immutable.Set<string>,
-        topicRead: Immutable.Map<string, { res: string, count: number }>) {
-        this.topicFavo = topicFavo;
-        this.topicRead = topicRead;
+    static fromJSON(jsonText: string): Storage {
+        if (jsonText.length !== 0) {
+            let JsonObj = JSON.parse(jsonText) as StorageJSON;
+            while (true) {
+                switch (JsonObj.ver) {
+                    case '0.0.0':
+                        // バージョン0
+                        JsonObj = this.convert1To2(JsonObj as StorageJSON1);
+                        break;
+                    case '2':
+                        {
+                            JsonObj = this.convert2To3(JsonObj as StorageJSON2);
+                            break;
+                        }
+                    case '3':
+                        {
+                            let obj = JsonObj as StorageJSON3;
+                            return new Storage(Immutable.Set(obj.topicFavo), Immutable.Map(obj.topicRead));
+                        }
+                    default:
+                        return new Storage(Immutable.Set<any>(), Immutable.Map<any, any>());
+                }
+            }
+        } else {
+            return new Storage(Immutable.Set<any>(), Immutable.Map<any, any>());
+        }
     }
 
-    private copy({topicFavo = this.topicFavo, topicRead = this.topicRead}: { topicFavo?: Immutable.Set<string>, topicRead?: Immutable.Map<string, { res: string, count: number }> }): Storage {
-        return new Storage(topicFavo, topicRead);
-    }
-
-    setTopicRead(topic: string, res: string, count: number): Storage {
-        return this.copy({ topicRead: this.topicRead.set(topic, { res, count }) });
-    }
-
-    setFavo(topicFavo: Immutable.Set<string>): Storage {
-        return this.copy({ topicFavo });
-    }
-
-    private static convert1_1_1To2(val: StorageJSON1_0_0): StorageJSON2 {
+    private static convert1To2(val: StorageJSON1): StorageJSON2 {
         return {
-            ver: "2",
+            ver: '2',
             topicFav: val.topicFav,
             topicRead: val.topicRead.map(x => {
                 return {
@@ -70,34 +78,23 @@ export class Storage {
     }
 
 
-    static fromJSON(json: string): Storage {
-        if (json.length !== 0) {
-            let obj = JSON.parse(json) as StorageJSON;
-            while (true) {
-                switch (obj.ver) {
-                    case "0.0.0":
-                        //バージョン0
-                        obj = this.convert1_1_1To2(obj as StorageJSON1_0_0);
-                        break;
-                    case "2":
-                        {
-                            obj = this.convert2To3(obj as StorageJSON2);
-                            break;
-                        }
-                    case "3":
-                        {
-                            let json = obj as StorageJSON3;
+    readonly topicFavo: Immutable.Set<string>;
+    readonly topicRead: Immutable.Map<string, { res: string, count: number }>;
 
-                            return new Storage(Immutable.Set(json.topicFavo), Immutable.Map(json.topicRead));
-                        }
-                    default:
-                        return new Storage(Immutable.Set<any>(), Immutable.Map<any, any>());
-                }
-            }
-        } else {
-            return new Storage(Immutable.Set<any>(), Immutable.Map<any, any>());
-        }
+    private constructor(topicFavo: Immutable.Set<string>,
+        topicRead: Immutable.Map<string, { res: string, count: number }>) {
+        this.topicFavo = topicFavo;
+        this.topicRead = topicRead;
     }
+
+    setTopicRead(topic: string, res: string, count: number): Storage {
+        return this.copy({ topicRead: this.topicRead.set(topic, { res, count }) });
+    }
+
+    setFavo(topicFavo: Immutable.Set<string>): Storage {
+        return this.copy({ topicFavo });
+    }
+
 
     toJSON(): string {
         let j: StorageJSON3 = {
@@ -107,5 +104,11 @@ export class Storage {
         };
 
         return JSON.stringify(j);
+    }
+
+
+    private copy({topicFavo = this.topicFavo, topicRead = this.topicRead}:
+        { topicFavo?: Immutable.Set<string>, topicRead?: Immutable.Map<string, { res: string, count: number }> }): Storage {
+        return new Storage(topicFavo, topicRead);
     }
 }
