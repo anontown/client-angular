cb082256a8c9f8e5dc6a2fd1293639c0728b517f
kgtkr <kgtkr.jp+program@gmail.com>
2017-01-04 19:41:18 +0900

    APIの仕様変更に対応

--- src/app/dialogs/topic-data/topic-data.component.ts
+++ src/app/dialogs/topic-data/topic-data.component.ts
@@ -1,23 +1,28 @@
-import { Component,
-   Input,
-    Output, 
-    EventEmitter, 
-    OnDestroy, 
-    OnInit ,
-  ChangeDetectionStrategy} from '@angular/core';
 import {
-  Topic
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import {
+  Topic,
+  History,
+  AtApiService
 } from 'anontown';
-import { UserService, 
+import {
+  UserService,
   IUserData,
-   IUserDataListener
-   } from '../../services';
-
+  IUserDataListener
+} from '../../services';
+import * as Immutable from 'immutable';
 
 @Component({
   selector: 'app-topic-data',
   templateUrl: './topic-data.component.html',
-    changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class TopicDataComponent implements OnInit, OnDestroy {
   @Input()
@@ -26,12 +31,16 @@ export class TopicDataComponent implements OnInit, OnDestroy {
   @Output()
   update = new EventEmitter<Topic>();
 
+  histories: Immutable.List<History>;
+
   ud: IUserData;
   private udListener: IUserDataListener;
-  constructor(private user: UserService) {
+  constructor(private user: UserService, private api: AtApiService) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.histories = Immutable.List(await this.api.findHistoryAll({ topic: this.topic.id }));
+
     this.udListener = this.user.addUserDataListener(ud => {
       this.ud = ud;
     });
