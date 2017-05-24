7f6b9af18d840b165f2fc70efd3475a963f0931a
kgtkr <kgtkr.jp+program@gmail.com>
2017-05-24 20:12:37 +0900

    時刻の相対表示

--- src/app/pipes/date.pipe.ts
+++ src/app/pipes/date.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'atDate' })
+@Pipe({ name: 'atDate', pure: false })
 export class AtDatePipe implements PipeTransform {
   transform(value: Date | string | number): any {
     let date: Date;
@@ -9,6 +9,29 @@ export class AtDatePipe implements PipeTransform {
     } else {
       date = new Date(value as any);
     }
+
+    let timespan = Date.now() - date.valueOf();
+
+    //一秒未満
+    if (timespan < 1000) {
+      return '現在';
+    }
+
+    //一分未満
+    if (timespan < 60 * 1000) {
+      return Math.floor(timespan / 1000) + '秒前';
+    }
+
+    //一時間未満
+    if (timespan < 60 * 60 * 1000) {
+      return Math.floor(timespan / 1000 / 60) + '分前';
+    }
+
+    //一日未満
+    if (timespan < 24 * 60 * 60 * 1000) {
+      return Math.floor(timespan / 1000 / 60 / 24) + '時間前';
+    }
+
     return ('0000' + date.getFullYear()).slice(-4)
       + '/'
       + ('00' + (date.getMonth() + 1)).slice(-2)
@@ -17,10 +40,6 @@ export class AtDatePipe implements PipeTransform {
       + '(' + ['日', '月', '火', '水', '木', '金', '土'][date.getDay()] + ') '
       + ('00' + date.getHours()).slice(-2)
       + ':'
-      + ('00' + date.getMinutes()).slice(-2)
-      + ':'
-      + ('00' + date.getSeconds()).slice(-2)
-      + '.'
-      + ('000' + date.getMilliseconds()).slice(-3)
+      + ('00' + date.getMinutes()).slice(-2);
   }
 }
