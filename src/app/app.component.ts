9dc903ea6364fc2f4b22bd3da2cabdd921bfc484
kgtkr <kgtkr.jp@gmail.com>
2017-10-08 13:36:36 +0900

    Revert "coin hive"
    This reverts commit ad9bb2214ea12905cd145b0550f05b806cb70b15.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,12 @@ import {
 import { Router } from '@angular/router';
 import { MdSnackBar, OverlayContainer } from '@angular/material';
 
-declare const CoinHive: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  isCoinHive = localStorage.getItem('iscoinhive') === 'true';
-  coinhive = new CoinHive.Anonymous('Zo2rps5kYDux3cSlc7viocgxGvTveti4');
-  changeCoinHive() {
-    if (this.isCoinHive) {
-      //無効にする
-      this.isCoinHive = false;
-      localStorage.setItem('iscoinhive', 'false');
-      this.coinhive.stop();
-      console.log("マイニング停止");
-    } else {
-      //有効にする
-      this.isCoinHive = true;
-      localStorage.setItem('iscoinhive', 'true');
-      this.coinhive.start();
-      console.log("マイニング開始");
-    }
-  }
-
   get isDarkTheme(): boolean {
     return document.body.classList.contains('app-dark-theme');
   }
@@ -63,10 +43,6 @@ export class AppComponent implements OnInit, OnDestroy {
     private overlayContainer: OverlayContainer) {
     setInterval(() => this.save(), 30 * 1000);
     this.isDarkTheme = localStorage.getItem('theme') === 'dark';
-    if (this.isCoinHive) {
-      this.coinhive.start();
-      console.log("マイニング開始");
-    }
   }
 
   changeTheme() {
--- src/app/config.ts
+++ src/app/config.ts
@@ -4,5 +4,4 @@ export class Config {
   static serverURL = environment.production ? 'https://api.anontown.com' : 'http://localhost:8080';
   static socketServerURL = environment.production ? 'wss://api.anontown.com' : 'ws://localhost:8080';
   static recaptcha = environment.production ? '6LdoFBQUAAAAACc3lhPhbkEANEAHsmNd6UDN2vKo' : '6LeoFBQUAAAAAB0fiXvXvaBO9VyFC7igegEOlD7a';
-  static coinhive = "Zo2rps5kYDux3cSlc7viocgxGvTveti4";
 }
